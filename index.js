748d53470635aea2c084ef7b79f416621751c443
mutemip <paulmutemi2030@gmail.com>
2022-06-27 17:18:17 +0300

    Sending Data with Fetch

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your code here
+
+
+function submitData(name, email){
+    
+    const configObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify({
+            name,
+            email
+        }),
+    }
+    return fetch("http://localhost:3000/users", configObject)
+    .then(res => res.json())
+    .then(data => document.body.innerHTML = data["id"])
+    .catch(error => document.body.innerHTML = error.message)
+}
